c0901a6fa8a97c1296a67a36d45b883582a55864
greg@gregPredator <greg19850@gmail.com>
2022-07-11 10:07:02 +0100

    Created function dding items to array and list, with use of conditions and loop for of)

--- script.js
+++ script.js
@@ -2,11 +2,23 @@ const input = document.querySelector('input');
 const btn = document.querySelector('button');
 const div = document.querySelector('div');
 
-const textArray = [];
+const items = [];
 
 const addElement = (event) => {
+  event.preventDefault();
   let textValue = input.value
 
-}
+  if (textValue.length) {
+    for (item of items) {
+      if (item === textValue) {
+        alert('Item already on the list!');
+        return;
+      }
+    }
+    items.push(textValue);
+    div.textContent += textValue + ', ';
+  }
+  input.value = '';
+};
 
 btn.addEventListener('click', addElement);
